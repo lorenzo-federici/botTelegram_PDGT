8ca2f956671b5a6321ed2032f957b65110a5e42c
Lorenzo <federici.l30@gmial.com>
2020-03-26 22:50:12 +0100

    aggiunto visualizzazione stazione piu vicina
    in piu ho aggiunto vari accorgimenti per milgiorare il codice

--- index.js
+++ index.js
@@ -8,7 +8,9 @@ const TKN_BOT = '1087756157:AAE0e2dgll0JliyCL8esAWQWFxgsyTMF8aE';
 const URL     = 'https://progetto-pdgt-federici.herokuapp.com'
 
 let where_i_am = "Start";  //FLAG status
-let TKN_Log   = ""
+let TKN_Log    = ""
+let header     = {}
+let updateOPT  = {}
 
 const bot = new TelegramBot(TKN_BOT, {polling: true});
 
@@ -62,21 +64,36 @@ const opts_view_station = {
 const opts_keyboard = {
     "reply_markup": {
         "keyboard": [
-            ["🚂View stations", "📍View station position"], ["➕Add station", "📝Modify station"], ["❌Delete station"]
+            ["🚂View stations", "📍View nearest station"], ["➕Add station", "📝Modify station"], ["❌Delete station"]
         ]
     }
 }
+const opts_location = {
+    "parse_mode": "Markdown",
+    "reply_markup": {
+        "one_time_keyboard": true,
+        "keyboard": [[{
+            text: "My location",
+            request_location: true
+        }], ["Cancel"]]
+    }
+};
 //-----------------------------------------------------------
 
+
 bot.onText(/\/start/, (msg, match) => {
-    where_i_am = "Start"
-    let txt = 'Welcome, to use my power register or Login: ';
-    bot.sendMessage(msg.chat.id, txt, opts_start);
+    if(where_i_am == 'Start'){
+        let txt = 'Welcome, to use my power register or Login: ';
+        bot.sendMessage(msg.chat.id, txt, opts_start);
+    }else{
+        let txt = 'You are allredy in.\nChoose what you want';
+        bot.sendMessage(msg.chat.id, txt, opts_keyboard);
+    }
 });
 
 bot.onText(/\/options/, (msg, match) => {
-    const chatId = msg.chat.id;
-    if(where_i_am == 'Inside'){
+    if(where_i_am != 'Start'){
+        where_i_am = 'Inside'
         let txt = 'Choose what you want'
         bot.sendMessage(msg.chat.id, txt, opts_keyboard)
     }else{
@@ -90,16 +107,16 @@ bot.on('callback_query', function onCallbackQuery(example){
     const msg = example.message
     
     if (action == 'Login' && where_i_am == 'Start'){
-        console.log('Login')
+        //console.log('Login')
         where_i_am = action;
         bot.sendMessage(msg.chat.id, "Tell me your email and password")
     }else if (action == 'Signup' && where_i_am == 'Start'){
-        console.log('Signup')
+        //console.log('Signup')
         where_i_am = action;
         bot.sendMessage(msg.chat.id, "Tell me your email and password so I'll register")
     }else if (action == 'all' && where_i_am == 'Inside'){
         // GET_ALL_STATIONS-------------------------------------------------------
-        axios.get(URL + '/stations')
+        axios.get(URL + '/stations/view/all')
         .then((response) => {
             getStation(msg.chat.id, response.data.count, response.data.stations)
         });
@@ -118,11 +135,12 @@ bot.on('callback_query', function onCallbackQuery(example){
 });
 
 bot.on('message', (msg) => {
+
     if (where_i_am == 'Login'){
         // LOGIN-----------------------------------------------------------------
         let userInfo = msg.text.toString().split(" ")
         axios.post(URL + '/users/login', {
-            email: userInfo[0].toLowerCase(),
+            email:    userInfo[0].toLowerCase(),
             password: userInfo[1]
         })
         .then((response) => {
@@ -131,7 +149,6 @@ bot.on('message', (msg) => {
             let txt1 = 'TOP you\'re in :)'
             let txt2 = '\n\nChoose what you want: '
             bot.sendMessage(msg.chat.id, txt1+txt2, opts_keyboard)
-            //console.log(tokenLog)
         },(error) => {
             where_i_am = "Start"
             let txt1 = 'Authentication failed try again:\n';
@@ -156,60 +173,60 @@ bot.on('message', (msg) => {
             bot.sendMessage(msg.chat.id, txt1+txt2, opts_start)
         });
         //------------------------------------------------------------------------
-    }else if (where_i_am == 'Inside' && msg.text.toString().includes("🚂View")){
-        let txt = 'Choose what you want to view the satations';
-        bot.sendMessage(msg.chat.id, txt, opts_view_station);
-    }else if (where_i_am == 'Inside' && msg.text.toString().includes("📍View")){
-        // PROOF GET_ONE_STATION--------------------------------------------------
-        let id_st = "5e6f9fbfa509220612ef49fc"
-        axios.get(URL + '/stations/'+id_st)
-        .then((response) => {
-            let txt = "Here is the point of " + response.data.station.Nome + "'s station:"
-            bot.sendMessage(msg.chat.id, txt);
-            bot.sendLocation(msg.chat.id, 
-                            response.data.station.Latitudine,
-                            response.data.station.Longitudine);   
-            //console.log(response.data.station._id)
-        });
-        //------------------------------------------------------------------------
+    }else if (where_i_am == 'Inside'){
 
-    }else if (where_i_am == 'Inside' && msg.text.toString().includes("➕Add")){
-        let txt1 = "Tell me all station's information you want to add, you will have to write me: "
-        let txt2 = "\nNAME\nMUNICIPALITY\nPROVINCE\nREGION\nLONGITUDE\nLATITUDE\nID_OpenStreetMap (if you know it)\n\n"
-        let txt3 = "You must write all infos divided by a space."
-        bot.sendMessage(msg.chat.id, txt1+txt2+txt3)
-        where_i_am = "➕Add";
-    }else if (where_i_am == 'Inside' && msg.text.toString().includes("📝Modify")){
-        // PROOF GET_PATCH_STATION--------------------------------------------------
-        //------------------------------------------------------------------------
-    }else if (where_i_am == 'Inside' && msg.text.toString().includes("❌Delete")){
-        where_i_am = "❌Delete";
+        if(msg.text.toString().includes("🚂View")){
+            let txt = 'Choose what you want to view the satations';
+            bot.sendMessage(msg.chat.id, txt, opts_view_station);
+        }else if(msg.text.toString().includes("📍View")){ 
+            bot.sendMessage(msg.chat.id, "Share me your position ", opts_location) 
+            where_i_am = "location"
+        }else if(msg.text.toString().includes("➕Add")){
+            let txt1 = "Tell me all station's information you want to add, you will have to write me: "
+            let txt2 = "\nNAME\nMUNICIPALITY\nPROVINCE\nREGION\nLONGITUDE\nLATITUDE\nID_OpenStreetMap (if you know it)\n\n"
+            let txt3 = "You must write all infos divided by a space."
+            bot.sendMessage(msg.chat.id, txt1+txt2+txt3)
+            where_i_am = "➕Add";
+        }else if(msg.text.toString().includes("📝Modify")){
+            where_i_am = "ask_update";
+    
+            let txt1 = "Tell me all OPTION's information you want to update, you will have to write me: "
+            let txt2 = "\nName\nMunicipality\nProvince\nRegion\nLongitude\nLatitude\nID_OpenStreetMap\n\n"
+            let txt3 = "You must write all infos divided by a space."
+            bot.sendMessage(msg.chat.id, txt1+txt2+txt3)
+        }else if(msg.text.toString().includes("❌Delete")){
+            where_i_am = "❌Delete";
 
-        let txt = "Tell me the station's id to be eliminated"
-        bot.sendMessage(msg.chat.id, txt)
+            let txt = "Tell me the station's id to be eliminated"
+            bot.sendMessage(msg.chat.id, txt)
+        }
+
     }else if (where_i_am == 'name'|| where_i_am == 'region' || where_i_am == 'province'){
         // GET_STATIONS-------------------------------------------------------
-        axios.get(URL + '/stations/'+where_i_am+'/'+msg.text.toString())
+        axios.get(URL + '/stations/view/'+where_i_am+'?prm='+msg.text.toString())
         .then((response) => {
             getStation(msg.chat.id, response.data.count, response.data.stations)
         });
         //-----------------------------------------------------------------------
         where_i_am = 'Inside'
     }else if(where_i_am == '➕Add'){
-        // PROOF GET_ADD_STATION--------------------------------------------------
+        where_i_am = "Inside";
+        // ADD_STATION-------------------------------------------------------------
         let stationInfo = msg.text.toString().split(" ")
-        let header = { headers: { 'Authorization': "bearer " + TKN_Log} }
-        let req = { Nome: stationInfo[0],
-            Comune: stationInfo[1],
-            Provincia: stationInfo[2],
-            Regione: stationInfo[3],
-            Longitudine: parseFloat(stationInfo[4]),
-            Latitudine: parseFloat(stationInfo[5])
+
+        header = { headers: { 'Authorization': "bearer " + TKN_Log} }
+        let req    = {  Nome: stationInfo[0],
+                    Comune: stationInfo[1],
+                    Provincia: stationInfo[2],
+                    Regione: stationInfo[3],
+                    Longitudine: parseFloat(stationInfo[4]),
+                    Latitudine: parseFloat(stationInfo[5])
         }
         //idopenmap it's unnecessary
         if(stationInfo.length == 7){
             req.ID_OpenStreetMap = parseInt(stationInfo[6])
         }
+
         axios.post(URL + '/stations/', req, header)
         .then((response) => {
             let station     = response.data.addedStation
@@ -221,7 +238,10 @@ bot.on('message', (msg) => {
             let txt = "Here is the new station: \n"
             bot.sendMessage(msg.chat.id, txt + station_txt)
             bot.sendLocation(msg.chat.id, station.Latitudine, station.Longitudine);
-        });
+        }) .catch( (error) =>  {
+            console.log(error);
+            bot.sendMessage(msg.chat.id, "Request erorr retry")
+        })
         //------------------------------------------------------------------------
     }else if (where_i_am == '❌Delete'){
         // PROOF GET_DELETE_STATION--------------------------------------------------
@@ -229,8 +249,7 @@ bot.on('message', (msg) => {
         
         axios.delete(URL + '/stations/'+msg.text.toString(), header)
         .then((response) => {
-            let txt = "Station successfully deleted"
-            bot.sendMessage(msg.chat.id, txt)
+            bot.sendMessage(msg.chat.id, response.data.message)
             where_i_am = "Inside"
         }).catch((error) => {
             if(error.response.data.message.toString().includes("token_error")){
@@ -245,10 +264,48 @@ bot.on('message', (msg) => {
 
         })
         //------------------------------------------------------------------------
-    }
-    
-});
+    }else if (where_i_am == 'ask_update'){
+        // Prepare the request
+        where_i_am = "📝Modify"
+        let stationInfo = msg.text.toString().split(" ")
+        let txt2 = "ID\n"
+        
+        for(let i = 0; i<stationInfo.length; i++){
+            updateOPT[i] = stationInfo[i]
+            txt2 = txt2 + stationInfo[i] + "\n"
+        }
+        
+        let txt = "Now Tell me the station's id to be update\n"
+        let txt1 = "and all STATIONS's information, you will have to write me: \n\n"
+        let txt3 = "\nYou must write all infos divided by a space."
+        bot.sendMessage(msg.chat.id, txt+txt1+txt2+txt3)
+    }else if (where_i_am == '📝Modify'){
+        where_i_am = "Inside";
+        let stationInfo = msg.text.toString().split(" ")
+        
+        header = { headers: { 'Authorization': "bearer " + TKN_Log} }
 
+        let req = {}
+        req.id = stationInfo[0]
+        
+        //stationInfo[0] is the ID
+        for(let i = 1; i<stationInfo.length; i++){
+            req[updateOPT[i-1]] = stationInfo[i]
+        }
+
+        //console.log(req)
+
+        axios.patch(URL + '/stations/' + stationInfo[0], req, header)
+        .then((response) => {
+            console.log(response.data)
+        })
+        .catch( (error) =>  {
+            console.log(error);
+            bot.sendMessage(msg.chat.id, "Request erorr retry")
+        })
+    }
+   
+});
 
 function getStation(idchat, n_stations, stations){
     let stations_txt = ""
@@ -256,14 +313,45 @@ function getStation(idchat, n_stations, stations){
         for(let i = 0; i < n_stations; i++){
         stations_txt =  stations_txt + (i+1) + ") " +
                         stations[i].Regione + 
-                        ": " + stations[i].Nome + " di " +
-                        stations[i].Comune + " \n\n"
+                        ": " + stations[i].Nome + " \n\n"
         }
         let txt = "Here are all the " + n_stations + " stations: \n\n" + stations_txt
         bot.sendMessage(idchat, txt)
-    }else{
+    }else if(n_stations = 1){
         let txt = "Here is the point of " + stations[0].Nome + "'s station"
         bot.sendMessage(idchat, txt);
         bot.sendLocation(idchat, stations[0].Latitudine, stations[0].Longitudine);
+    }else{
+        let txt = "No station found"
+        bot.sendMessage(idchat, txt);
     }
 }
+
+// Get Location-----------------------------------------
+bot.on('location', (msg) => {
+    let latitude = msg.location.latitude
+    let longitude = msg.location.longitude
+
+    //console.log(latitude)
+    //console.log(longitude)
+
+    where_i_am = 'Inside'
+
+    let urlview = URL+'/stations/near/?lat='+latitude+'&long='+longitude
+    axios.get(urlview)
+    .then((response) =>{
+        //console.log(response.data);
+        let txt = response.data.Nome + "'s station is the one closest to you.\nDistance: " + response.data.Distanza
+        bot.sendMessage(msg.chat.id, txt);
+        bot.sendLocation(msg.chat.id, response.data.Latitudine, response.data.Longitudine)
+    })
+    .catch( (error) =>  {
+        console.log(error);
+        bot.sendMessage(msg.chat.id, "Request erorr retry")
+    })
+    
+});
+//------------------------------------------------------
+
+
+
